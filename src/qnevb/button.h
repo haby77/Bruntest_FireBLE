2c43c0576b2e694b346911a3bfeecbaff2098fa3
hab <hab@t-chip.com.cn>
2015-01-29 16:22:16 +0800

    Init burn_in_test project

---
+++ src/qnevb/button.h
@@ -0,0 +1,43 @@
+/**
+ ****************************************************************************************
+ *
+ * @file button.h
+ *
+ * @brief Header file of button driver for qn evb.
+ *
+ * Copyright (C) Quintic 2012-2013
+ *
+ * $Rev: 1.0 $
+ *
+ ****************************************************************************************
+ */
+#ifndef _BUTTON_H_
+#define _BUTTON_H_
+
+/*
+ * MACRO DEFINITIONS
+ ****************************************************************************************
+ */
+ #if !defined(QN_9021_MINIDK)
+   #if !defined(FireBlue_platform)
+     #define BUTTON1_PIN    (GPIO_P14)
+     #define BUTTON2_PIN    (GPIO_P15)
+   #else
+     #define	BUTTON1_PIN	 (GPIO_P12)
+     #define	BUTTON2_PIN	 (GPIO_P02)  // no pin in QN9021
+   #endif
+ #else
+   #define BUTTON1_PIN    (GPIO_P12)
+   #define BUTTON2_PIN    (GPIO_P10)
+ #endif
+
+/*
+ * FUNCTION DECLARATIONS
+ ****************************************************************************************
+ */
+
+extern void button_init(void);
+extern int check_button_state(int btn);
+
+#endif
+
